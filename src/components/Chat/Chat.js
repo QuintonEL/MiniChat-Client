1a4c87fedc44843d1391e46dac787247d9876ce6
QuintonEL <quintonlaborde@gmail.com>
2020-04-11 13:44:21 -0600

    now shows active users in room, everyting apprears to be working fine

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -14,6 +14,7 @@ let socket;
 const Chat = ({ location }) => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
+  const [users, setUsers] = useState('')
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
   const ENDPOINT = 'localhost:5000';
@@ -26,8 +27,10 @@ const Chat = ({ location }) => {
     setName(name);
     setRoom(room);
 
-    socket.emit('join', { name, room }, () => {
-  
+    socket.emit('join', { name, room }, (error) => {
+      if(error) {
+        alert(error);
+      }
     });
 
     return () => {
@@ -37,11 +40,14 @@ const Chat = ({ location }) => {
     }
   }, [ENDPOINT, location.search]);
 
-  useEffect(() => {
-    socket.on('message', (message) => {
-      setMessages([...messages, message]);
-    })
-  }, [messages]);
+  useEffect(( ) => {
+    socket.on("message", message => {
+      setMessages(msgs => [...msgs, message]);
+    });
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+  }, [ ]);
 
   const sendMessage = (event) => {
     event.preventDefault();
